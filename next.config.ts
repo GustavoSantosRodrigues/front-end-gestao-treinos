0266e0ed7ad95a1856e35e8c9f8c567f0ffb8eba
GustavoSantosRodrigues <s.rodrigues.2050@hotmail.com>
2026-03-18 17:40:18 -0300

    git commit -m "feat(pwa): configura next-pwa com service worker e skipWaiting false"

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
+import withPWA from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
@@ -12,4 +12,10 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  register: true,
+  workboxOptions: {
+    skipWaiting: false,
+  },
+})(nextConfig);
